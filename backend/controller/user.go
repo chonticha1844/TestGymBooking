3e1392e1c649fb32c7c75e32e47180b8b0346483
chonticha1844 <111478571+chonticha1844@users.noreply.github.com>
2023-07-07 16:37:04 +0700

    controller user

---
+++ backend/controller/user.go
@@ -0,0 +1,154 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/chonticha1844/TestGymBooking/entity"
+	//"github.com/asaskevich/govalidator"
+	"github.com/gin-gonic/gin"
+	//"golang.org/x/crypto/bcrypt"
+	//"gorm.io/gorm"
+)
+
+// POST /writers
+
+func CreateUser(c *gin.Context) {
+
+	var user entity.User
+	var gender entity.Gender
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// ค้นหา gender ด้วย id
+	if tx := entity.DB().Where("id = ?", user.GenderID).First(&gender); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "กรุณาเลือกเพศ"})
+		return
+	}
+
+	// // เข้ารหัสลับรหัสผ่านที่ผู้ใช้กรอกก่อนบันทึกลงฐานข้อมูล
+	// hashPassword, err := bcrypt.GenerateFromPassword([]byte(writer.Password), 14)
+	// if err != nil {
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": "error hashing password"})
+	// 	return
+	// }
+
+	// 14: สร้าง  user
+	usr := entity.User{
+		Username: user.Username,
+		Gmail:    user.Gmail,
+		// Password:        string(hashPassword),
+		Gender:   gender,
+		Fullname: user.Fullname,
+		Age:      user.Age,
+		Weight:   user.Weight,
+		Height:   user.Height,
+	}
+
+	// // การ validate
+	// if _, err := govalidator.ValidateStruct(user); err != nil {
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	// 	return
+	// }
+
+	// 13: บันทึก
+	if err := entity.DB().Create(&usr).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"data": usr})
+
+}
+
+// GET /user/:id
+func GetUser(c *gin.Context) {
+	var user entity.User
+	id := c.Param("id")
+	if tx := entity.DB().Preload("Gender").Raw("SELECT * FROM users WHERE id = ?", id).Find(&user).Error; tx != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": user})
+}
+
+// GET /user
+func ListUsers(c *gin.Context) {
+	var users []entity.User
+
+	if err := entity.DB().Preload("Gender").Raw("SELECT * FROM Users").Find(&users).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": users})
+}
+
+func UpdateUser(c *gin.Context) {
+	var user entity.User
+	var gender entity.Gender
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// ค้นหา gender ด้วย id
+	if tx := entity.DB().Where("id = ?", user.GenderID).First(&gender); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "กรุณาเลือกเพศ"})
+		return
+	}
+
+	update_user := entity.User{
+		Username: user.Username,
+		Gmail:    user.Gmail,
+		// Password:        string(hashPassword),
+		Gender:   gender,
+		Fullname: user.Fullname,
+		Age:      user.Age,
+		Weight:   user.Weight,
+		Height:   user.Height,
+	}
+
+	// // การ validate
+	// if _, err := govalidator.ValidateStruct(update_user); err != nil {
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	// 	return
+	// }
+
+	// if !(user.Password[0:13] == "$2a$14$") {
+	// 	hashPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+	// 	if err != nil {
+	// 		c.JSON(http.StatusBadRequest, gin.H{"error": "error hashing password"})
+	// 		return
+
+	// 	}
+	// 	update_user.Password = string(hashPassword)
+	// }
+
+	if tx := entity.DB().Where("id = ?", user.ID).Updates(&update_user).Error; tx != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": tx.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": update_user})
+
+}
+func DeleteUser(c *gin.Context) {
+	id := c.Param("id")
+
+	//ลบเมื่อ
+	if err := entity.DB().Exec("DELETE FROM fictions WHERE user_id = ?", id).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if tx := entity.DB().Exec("DELETE FROM users WHERE id = ?", id); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "users not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": id})
+
+}
