e2d52e66b0bad282efc5a6e98ed61b6c804d3c2e
chonticha1844 <111478571+chonticha1844@users.noreply.github.com>
2023-07-07 16:42:51 +0700

    controller equipment

---
+++ backend/controller/equipment.go
@@ -0,0 +1,75 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/chonticha1844/TestGymBooking/entity"
+	"github.com/gin-gonic/gin"
+)
+
+// POST--equipment--
+func CreateEquipment(c *gin.Context) {
+	var equipment entity.Equipment
+	if err := c.ShouldBindJSON(&equipment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := entity.DB().Create(&equipment).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": equipment})
+}
+
+//GET--equipment id--
+
+func GetEquipment(c *gin.Context) {
+	var equipment entity.Equipment
+	id := c.Param("id")
+	if err := entity.DB().Raw("SELECT * FROM equipments WHERE id = ?", id).Scan(&equipment).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": equipment})
+}
+
+// GET--equipments--
+func ListEquipments(c *gin.Context) {
+	var equipments []entity.Equipment
+	if err := entity.DB().Raw("SELECT * FROM equipments").Scan(&equipments).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": equipments})
+}
+
+// DELETE--equipment id--
+func DeleteEquipment(c *gin.Context) {
+	id := c.Param("id")
+	if tx := entity.DB().Exec("DELETE FROM equipments WHERE id = ?", id); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "equipment not found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": id})
+}
+
+// PATCH--equipment--
+func UpdateEquipment(c *gin.Context) {
+	var equipment entity.Equipment
+	if err := c.ShouldBindJSON(&equipment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if tx := entity.DB().Where("id = ?", equipment.ID).First(&equipment); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "equipment not found"})
+		return
+	}
+
+	if err := entity.DB().Save(&equipment).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": equipment})
+}
