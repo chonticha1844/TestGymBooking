e4666f3a0d5710d3f45efec1914352972dd36423
chonticha1844 <111478571+chonticha1844@users.noreply.github.com>
2023-07-07 15:24:14 +0700

    Entity

--- backend/entity/schema.go
+++ backend/entity/schema.go
@@ -1,16 +1,54 @@
 package entity
 
 import (
-	//"time"
-
 	//"regexp"
 
 	//"github.com/asaskevich/govalidator"
+	"time"
+
 	"gorm.io/gorm"
 )
 
-// ---Education---
-type Education struct {
+// Equipment
+type Equipment struct {
 	gorm.Model
-	Education_degree string
+	Equipments string
+
+	Reservation []Reservation `gorm:"foreignKey:ReservationID"`
+}
+
+// Gender
+type Gender struct {
+	gorm.Model
+	Sex string
+
+	User []User `gorm:"foreignKey:UserID"`
+}
+
+// User
+type User struct {
+	gorm.Model
+	Username string
+	Gmail    string
+	Password string
+	Fullname string
+	Age      int32
+	Weight   int32
+	Height   int32
+
+	GenderID *uint
+	Gender   Gender
+
+	Reservation []Reservation `gorm:"foreignKey:ReservationID"`
+}
+
+// Reservation
+type Reservation struct {
+	gorm.Model
+	Datetime time.Time
+
+	UserID      *uint
+	User        User
+	EquipmentID *uint
+	Equipment   Equipment
 }
--- backend/entity/setup.go
+++ backend/entity/setup.go
@@ -1,8 +1,9 @@
 package entity
 
 import (
-	// "fmt"
-	//"time"
+	//"fmt"
+	//"go/format"
+	"time"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -15,9 +16,158 @@ func DB() *gorm.DB {
 }
 
 func SetupDatabase() {
-	database, err := gorm.Open(sqlite.Open("se-65.db"), &gorm.Config{})
+	database, err := gorm.Open(sqlite.Open("project.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
-	database.AutoMigrate()
+	database.AutoMigrate(
+		&Equipment{},
+		Gender{},
+		&User{},
+		&Reservation{},
+	)
+
+	db = database
+
+	//Equipment
+	torso_twist := Equipment{
+		Equipments: "Torso Twist",
+	}
+	db.Model(&Equipment{}).Create(torso_twist)
+
+	ABD := Equipment{
+		Equipments: "ABD.Machine",
+	}
+	db.Model(&Equipment{}).Create(ABD)
+
+	leg_press := Equipment{
+		Equipments: "Leg Press",
+	}
+	db.Model(&Equipment{}).Create(leg_press)
+
+	leg_extension := Equipment{
+		Equipments: "Leg Extension",
+	}
+	db.Model(&Equipment{}).Create(leg_extension)
+
+	leg_curl := Equipment{
+		Equipments: "Leg Curl",
+	}
+	db.Model(&Equipment{}).Create(leg_curl)
+
+	calf_raise := Equipment{
+		Equipments: "Calf Raise",
+	}
+	db.Model(&Equipment{}).Create(calf_raise)
+
+	in_thing := Equipment{
+		Equipments: "Inner Thigh",
+	}
+	db.Model(&Equipment{}).Create(in_thing)
+
+	o_thing := Equipment{
+		Equipments: "Outer Thigh",
+	}
+	db.Model(&Equipment{}).Create(o_thing)
+
+	pec_deck := Equipment{
+		Equipments: "Pec Deck",
+	}
+	db.Model(&Equipment{}).Create(pec_deck)
+
+	in_press := Equipment{
+		Equipments: "Incline Press",
+	}
+	db.Model(&Equipment{}).Create(in_press)
+
+	chest_press := Equipment{
+		Equipments: "Chest Press",
+	}
+	db.Model(&Equipment{}).Create(chest_press)
+
+	sh_press := Equipment{
+		Equipments: "Shoulder Press",
+	}
+	db.Model(&Equipment{}).Create(sh_press)
+
+	lat_pull := Equipment{
+		Equipments: "Lat Pulldown",
+	}
+	db.Model(&Equipment{}).Create(lat_pull)
+
+	seated_row := Equipment{
+		Equipments: "Seated Row",
+	}
+	db.Model(&Equipment{}).Create(seated_row)
+
+	back_ex := Equipment{
+		Equipments: "Back Extention",
+	}
+	db.Model(&Equipment{}).Create(back_ex)
+
+	lat_row := Equipment{
+		Equipments: "Lat/High Row",
+	}
+	db.Model(&Equipment{}).Create(lat_row)
+
+	arm_curl := Equipment{
+		Equipments: "Arm Curl",
+	}
+	db.Model(&Equipment{}).Create(arm_curl)
+
+	tri_ex := Equipment{
+		Equipments: "Triceps Extension",
+	}
+	db.Model(&Equipment{}).Create(tri_ex)
+
+	//gender
+	male := Gender{
+		Sex: "Male",
+	}
+	db.Model(&Gender{}).Create(male)
+
+	female := Gender{
+		Sex: "Female",
+	}
+	db.Model(&Gender{}).Create(female)
+
+	//User
+	user1 := User{
+		Username: "B1234567",
+		Gmail:    "B1234567@g.sut.ac.th",
+		Password: "1234567890123",
+		Fullname: "Tom Highway",
+		Gender:   male,
+		Age:      21,
+		Weight:   70,
+		Height:   182,
+	}
+	db.Model(&User{}).Create(user1)
+
+	user2 := User{
+		Username: "D1472583",
+		Gmail:    "D1472583@g.sut.ac.th",
+		Password: "2345678901234",
+		Fullname: "Malisa Somalia",
+		Gender:   female,
+		Age:      30,
+		Weight:   56,
+		Height:   160,
+	}
+	db.Model(&User{}).Create(user2)
+
+	//Reservation
+	resv1 := Reservation{
+		User:      user1,
+		Datetime:  time.Date(2023, 5, 10, 15, 30, 00, 00, time.Now().Local().Location()),
+		Equipment: leg_press,
+	}
+	db.Model(&Reservation{}).Create(resv1)
+
+	resv2 := Reservation{
+		User:      user2,
+		Datetime:  time.Date(2023, 5, 10, 16, 00, 00, 00, time.Now().Local().Location()),
+		Equipment: leg_press,
+	}
+	db.Model(&Reservation{}).Create(resv2)
 }
